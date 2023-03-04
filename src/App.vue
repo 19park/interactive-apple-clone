9c94657ea055db5d729dbf28f3ca1b1ff937ccd0
parkjunghwan-reco <110369451+parkjunghwan-reco@users.noreply.github.com>
2023-03-04 17:54:50 +0900

    3번째 캔버스 추가, 스케일 조정

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,7 @@ onMounted(init);
         쾌적한 환경을 조성하고<br/>
         양질의 폐자원을 배출합니다.
       </p>
+      <canvas class="image-blend-canvas" width="1920" height="1080"></canvas>
       <p class="canvas-caption">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae est ipsa minima, eligendi error cum vel dolorum pariatur officia facilis ipsam voluptatibus ad quasi porro quod quisquam quidem tempora accusantium accusamus, quaerat aliquam velit exercitationem incidunt? Id vitae quisquam saepe quasi accusantium tempore enim! Aperiam dolorum a vero repellat dolor, inventore ab odit totam molestias expedita? Enim quia dolor maiores veniam ea! Quam illo, est incidunt ipsa reiciendis modi quisquam reprehenderit fuga velit dolorem odit sequi autem blanditiis, ullam commodi quibusdam. Accusamus repellat aperiam quis neque laudantium, dignissimos hic nisi magnam praesentium enim beatae sint architecto cum numquam inventore rerum animi sed nostrum quae delectus, voluptas molestiae placeat aliquid! Vel quaerat error officiis magnam dolorum iste aspernatur at est! Quo, consequuntur? Reiciendis, dolor. Quo at cupiditate in iure obcaecati voluptatum vel ea! Ab vel harum facere hic fuga ducimus sapiente dolore dolorem, nobis sint perferendis cumque esse! Omnis fugiat sint error laborum eveniet labore nam ducimus quisquam in repudiandae impedit excepturi dignissimos tenetur libero placeat rerum maxime tempore, aut nihil. Qui, quam? Voluptate fuga possimus itaque quas nesciunt iste, facilis mollitia illo qui placeat temporibus inventore obcaecati. Recusandae, sequi dignissimos in natus eum maiores dolorem, deleniti nobis accusantium, aspernatur beatae.
       </p>
@@ -140,8 +141,9 @@ onMounted(init);
   }
 
   .desc-message {
-    width: 50%;
+    width: 45%;
     font-weight: bold;
+    opacity: 0;
 
     .pin {
       width: 1px;
@@ -194,6 +196,11 @@ onMounted(init);
       left: 45%;
     }
   }
+  &#scroll-section-3 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 
 #show-scene-0 #scroll-section-0 .sticky-elem,
@@ -201,6 +208,7 @@ onMounted(init);
 #show-scene-2 #scroll-section-2 .sticky-elem,
 #show-scene-3 #scroll-section-3 .sticky-elem {
   display: block;
+  will-change: transform, opacity;
 }
 
 footer {
@@ -239,6 +247,8 @@ footer {
       font-size: 4vw;
     }
     .canvas-caption {
+      margin-top: -8rem;
+      padding: 0;
       font-size: 2rem;
     }
 
--- src/scene.js
+++ src/scene.js
@@ -100,7 +100,14 @@ export function init() {
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3'),
-                canvasCaption: document.querySelector('.canvas-caption')
+                canvasCaption: document.querySelector('.canvas-caption'),
+                canvas: document.querySelector('.image-blend-canvas'),
+                content: document.querySelector('.image-blend-canvas').getContext('2d'),
+                imagesPath: [
+                    '/images/blend-image-1.jpg',
+                    '/images/blend-image-2.png',
+                ],
+                images: []
             },
             values: {
 
@@ -120,6 +127,13 @@ export function init() {
             imgElem2 = new Image();
             imgElem2.src = `/video/002/IMG_${7027 + i}.JPG`;
             sceneInfo[2].objs.videoImages.push(imgElem2);
+        }
+
+        let imgElem3;
+        for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
+            imgElem3 = new Image();
+            imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+            sceneInfo[3].objs.images.push(imgElem3);
         }
     }
     setCanvasImages();
@@ -288,6 +302,21 @@ export function init() {
 
             case 3:
                 // console.log('3 play');
+                // 가로세로 채우기위해 여기서 세팅
+                const widthRatio = window.innerWidth / objs.canvas.width;
+                const heightRatio = window.innerHeight / objs.canvas.height;
+                let canvasScaleRatio;
+
+                if (widthRatio <= heightRatio) {
+                    // 캔버스보다 브라우저창이 훌쭉한경우
+                    canvasScaleRatio = heightRatio;
+                } else {
+                    // 캔버스보다 브라우저창이 납작한경우
+                    canvasScaleRatio = widthRatio;
+                }
+                objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+                objs.content.drawImage(objs.images[0], 0, 0);
+
                 break;
         }
     }
